1f400675e7ca9bf9553d89c708b25668096234ea
Brian T. <bptiburcio@gmail.com>
2024-08-08 10:34:10 -0400

    Fixes flex row in index.css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-
-function App() {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+export function App() {
+	return (
+		<div className="w-full">
+			<h1 className="text-9xl font-bold underline bg-red-500">Hello world!</h1>
+		</div>
+	);
 }
 
 export default App;
