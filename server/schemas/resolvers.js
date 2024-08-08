3b3707ee0c74aff33f43851dc9819f84ca323789
Jakostein97 <jacques.floresgarcia@gmail.com>
2024-08-08 11:42:29 -0400

    Adds user model

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,30 +1,9 @@
-const { User, Product, Category, Order } = require('../models');
+const { User } = require('../models');
 const { signToken, AuthenticationError } = require('../utils/auth');
 const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 const resolvers = {
   Query: {
-    categories: async () => {
-      return await Category.find();
-    },
-    products: async (parent, { category, name }) => {
-      const params = {};
-
-      if (category) {
-        params.category = category;
-      }
-
-      if (name) {
-        params.name = {
-          $regex: name
-        };
-      }
-
-      return await Product.find(params).populate('category');
-    },
-    product: async (parent, { _id }) => {
-      return await Product.findById(_id).populate('category');
-    },
     user: async (parent, args, context) => {
       if (context.user) {
         const user = await User.findById(context.user._id).populate({
