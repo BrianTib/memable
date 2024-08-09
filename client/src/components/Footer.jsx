146faf0d74ff167205f04adfa2edb6d6f98fa6c0
Julia Eason <jmeason00@gmail.com>
2024-08-09 11:19:19 -0400

    fixing content

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 export default function Component() {
   return (
     <footer className="bg-white text-gray-600 py-6">
-  <div className="container mx-auto flex justify-between items-center">
+  <div className="mx-auto flex md:flex-col justify-between items-center">
     <p className="text-sm">&copy; 2024 Memable. All rights reserved.</p>
-    <div className="text-sm flex space-x-4">
+    <div className="text-sm flex md:flex-col space-x-4">
       <a href="#" className="text-gray-600 hover:text-black">Privacy Policy</a>
       <a href="#" className="text-gray-600 hover:text-black">Terms of Service</a>
     </div>
