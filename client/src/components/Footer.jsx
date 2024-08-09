ab3d54f16b7b24a63e3113e91a863fd09137d07a
Julia Eason <jmeason00@gmail.com>
2024-08-09 11:28:36 -0400

    making the footer mobile responsive

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,20 +2,15 @@ import React from 'react';
 
 export default function Component() {
   return (
-    <footer className="bg-gray-800 text-white py-6">
-      <div className="container mx-auto text-center">
-        <p className="text-sm">&copy; 2024 Memable. All rights reserved.</p>
-        <div className="mt-4">
-          <a href="#" className="text-gray-400 hover:text-white mx-2">Privacy Policy</a>
-          <a href="#" className="text-gray-400 hover:text-white mx-2">Terms of Service</a>
-          <a href="#" className="text-gray-400 hover:text-white mx-2">Contact Us</a>
-        </div>
-        <div className="mt-4">
-          <a href="#" className="text-gray-400 hover:text-white mx-2">Facebook</a>
-          <a href="#" className="text-gray-400 hover:text-white mx-2">Twitter</a>
-          <a href="#" className="text-gray-400 hover:text-white mx-2">LinkedIn</a>
-        </div>
-      </div>
-    </footer>
+    <footer className="bg-white text-gray-600 py-2">
+  <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+    <p className="text-sm mb-2 md:mb-0">&copy; 2024 Memable. All rights reserved.</p>
+    <div className="text-sm flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4">
+      <a href="#" className="text-gray-600 hover:text-black">Privacy Policy</a>
+      <a href="#" className="text-gray-600 hover:text-black">Terms of Service</a>
+    </div>
+  </div>
+</footer>
+
   );
 }
