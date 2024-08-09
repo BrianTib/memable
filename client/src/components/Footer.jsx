e0a04e49cc596043d9de950c3c6cfa5c75303c93
Julia Eason <jmeason00@gmail.com>
2024-08-09 10:56:54 -0400

    fixing the contents in the footer and the styling

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,18 +2,12 @@ import React from 'react';
 
 export default function Component() {
   return (
-    <footer className="bg-gray-800 text-white py-6">
-      <div className="container mx-auto text-center">
+    <footer className= "bg-white text-black py-6">
+      <div className="container my-3 text-center">
         <p className="text-sm">&copy; 2024 Memable. All rights reserved.</p>
         <div className="mt-4">
-          <a href="#" className="text-gray-400 hover:text-white mx-2">Privacy Policy</a>
-          <a href="#" className="text-gray-400 hover:text-white mx-2">Terms of Service</a>
-          <a href="#" className="text-gray-400 hover:text-white mx-2">Contact Us</a>
-        </div>
-        <div className="mt-4">
-          <a href="#" className="text-gray-400 hover:text-white mx-2">Facebook</a>
-          <a href="#" className="text-gray-400 hover:text-white mx-2">Twitter</a>
-          <a href="#" className="text-gray-400 hover:text-white mx-2">LinkedIn</a>
+          <a href="#" className="text-gray-600 hover:text-white mx-2">Privacy Policy</a>
+          <a href="#" className="text-gray-600 hover:text-white mx-2">Terms of Service</a>
         </div>
       </div>
     </footer>
