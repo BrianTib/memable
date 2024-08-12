21b91c1e0a19b784525d4396a0a7328959838dc5
cowboytrex <careesechow@gmail.com>
2024-08-12 22:37:40 +0800

    top memes style

--- client/src/pages/TopMemes.jsx
+++ client/src/pages/TopMemes.jsx
@@ -43,3 +43,8 @@ const TopMemes = () => {
           >
             Year
           </button>
+          </div>
+
+{/* Meme Grid */}
+<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+  {/* Placeholder for dynamic content */}
