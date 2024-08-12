0556ac4df688a19f6289bb64de48d79bd12bf8e3
Julia Eason <jmeason00@gmail.com>
2024-08-12 19:33:01 -0400

    adding background color

--- client/src/pages/TopMemes.jsx
+++ client/src/pages/TopMemes.jsx
@@ -38,11 +38,23 @@ const TopMemes = () => {
   };
 
   return (
-    <div className="flex flex-col min-h-screen bg-white text-gray-900">
+    <div>
+      <section className="bg-[#55883A] relative py-10">
+                    <svg
+                    viewBox="0 0 1440 58"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="100%"
+                    className="absolute top-0 bg-transparent">
+                    <path
+                        d="M-100 58C-100 58 218.416 36.3297 693.5 36.3297C1168.58 36.3297 1487 58 1487 58V-3.8147e-06H-100V58Z"
+                        fill="#fff"></path>
+                </svg>
+    <div className="flex flex-col min-h-screen text-gray-900">
       {/* Main Content */}
       <main className="flex-grow px-5 py-10">
         <h2 className="text-3xl font-bold text-center">Top Memes</h2>
-        <p className="text-center text-lg text-gray-600 mb-10">
+        <p className="text-center text-lg text-white mb-10">
           Explore the funniest memes from today, this week, this month, and this year!
         </p>
 
@@ -93,7 +105,8 @@ const TopMemes = () => {
           ))}
         </div>
       </main>
-
+      </div>
+      </section>
     </div>
   );
 };
